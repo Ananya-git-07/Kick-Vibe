72d2ed68ca7ef5abf4a484ad98e0bf51c21e9c64
Ananya Tiwari <ananyatiwari3004@gmail.com>
2025-12-01 06:07:04 +0000

    Improve

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -177,6 +177,15 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 // Controller to change the user's password
 const changeCurrentPassword = asyncHandler(async (req, res) => {
     const { oldPassword, newPassword } = req.body;
+    
+    if (!oldPassword || !newPassword) {
+        throw new ApiError(400, "Both old password and new password are required");
+    }
+    
+    if (newPassword.length < 6) {
+        throw new ApiError(400, "New password must be at least 6 characters long");
+    }
+    
     const user = await User.findById(req.user?._id);
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
 
